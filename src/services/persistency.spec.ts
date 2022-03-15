9360e284de5fb26d2e08007dcc1aa893cf2a53f0
Jailson Prado <jailsondpd@gmail.com>
2022-03-15 16:12:32 -0300

    criando novo teste para class messaging

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,16 +1,28 @@
-describe('testando alguma coisa', () => {
-  it('Should return one', () => {
-    const number = 1
-  
-    expect(number).toBe(1)
+import { Persistency } from './persistency';
+
+describe('Persistency' ,() => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('should  return undefined', () => {
+    // sut : System under test
+    const sut = new Persistency()
+    expect(sut.saveOrder()).toBeUndefined()
   });
-})
 
+  it('should call console.log once', () => {
+    // sut : System under test
+    const sut = new Persistency()
+    const consoleSpy = jest.spyOn(console, 'log')
 
-describe('testando outra coisa', () => {
-test('Should return Jailson)', () => {
-  const nome = 'Jailson'
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    // sut : System under test
+    const sut = new Persistency()
+    const consoleSpy = jest.spyOn(console, 'log')
 
-  expect(nome).toBe('Jailson')
-})
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+  });
 })
